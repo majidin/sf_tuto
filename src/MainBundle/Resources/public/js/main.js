29f5470b1b14f5ecef8a245ce8962d47489a9d8f
majidin <abdelmejid.alouatiq@gmail.com>
2016-11-02 20:25:44 +0200

    Adding font-awesome & Cosmo Theme & Base Block decomposition

---
+++ src/MainBundle/Resources/public/js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+      $('body').append('<div id="toTop" class="btn btn-primary"><span class="glyphicon glyphicon-chevron-up"></span></div>');
+    	$(window).scroll(function () {
+			if ($(this).scrollTop() >= 100) {
+				$('#toTop').fadeIn();
+			} else {
+				$('#toTop').fadeOut();
+			}
+		}); 
+    $('#toTop').click(function(){
+        $("html, body").animate({ scrollTop: 0 }, 600);
+        return false;
+    });
+});
